265c504697fed8ab7ab9e41da6ca4202435c50de
arnobtarafder <rmtarnob@gmail.com>
2022-04-01 13:51:40 +0600

    regular commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import TShirt from '../TShirt/TShirt';
 
-const Cart = () => {
+const Cart = ({cart, handleRemoveFromCart}) => {
+    // Conditional rendering options 
+    // 1. Element variable
+    let command;
+    if(cart.length === 0){
+        command = <p>Please add some items !!!</p>
+    }
+    else if(cart.length === 1) {
+        command = <p>please add more...</p>
+    }
+    else {
+        command = <p><small>Thanks for adding item</small></p>
+    }
     return (
         <div>
-            <h1>This is Cart</h1>
+            <h3>Item Selected: {cart.length}</h3>
+            {
+                cart.map(tshirt => <p>
+                    {tshirt.name}
+                    <button onClick={() => handleRemoveFromCart(tshirt)}>Remove</button>
+                    </p>)
+            }
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from '../Cart/Cart';
 import useTShirts from '../hooks/useTShirts';
+import TShirt from '../TShirt/TShirt';
 import './Home.css'
 
 const Home = () => {
     const [tshirts, setTshirts] = useTShirts();
+    const [cart, setCart] = useState([]);
+
+    const handleAddToCart = (selected) => {
+        const exists = cart.find(tshirt => tshirt._id === selected._id)
+        if(!exists) {
+            const newCart = [...cart, selected];
+            setCart(newCart)
+        }
+        else{
+            // alert("already added")
+        }
+    }
+
+    const handleRemoveFromCart = (selected) => {
+        const rest = cart.filter(tshirt => tshirt._id !== selected._id)
+        setCart(rest)
+    } 
+
     return (
         <div className='home-container'>
             <div className="tshirt-container">
-                <h3>{tshirts.length}</h3>
+                {/* <h3>{tshirts.length}</h3> */}
+
+                {
+                    tshirts.map(tshirt => <TShirt
+                        key = {tshirt._id}
+                        tshirt = {tshirt}
+                        handleAddToCart = {handleAddToCart}
+                    ></TShirt>)
+                }
             </div>
             <div className="cart-container">
-                <Cart></Cart>
+                <Cart 
+                cart = {cart}
+                handleRemoveFromCart = {handleRemoveFromCart}
+                ></Cart>
             </div>
         </div>
     );
